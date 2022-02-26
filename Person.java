7b7670eaf2c905e393c7d57248c8a6200ab26bbd
Ларионов Георгий Юрьевич <zoralarionov@gmail.com>
2022-02-26 18:56:27 +0300

    update Person

--- Person.java
+++ Person.java
@@ -1,7 +1,12 @@
 public class Person {
     private String name;
+    private String secondname;
+    private String age;
 
-    public Person(String name) {
+
+    public Person(String name , String age , String secondname) {
+        this.secondname = secondname;
         this.name = name;
+        this.age = age;
     }
 }
